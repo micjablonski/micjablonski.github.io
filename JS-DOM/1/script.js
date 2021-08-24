74dc93dd27007e689488e6e796c9eb8faf4f1460
micjablonski <87521914+micjablonski@users.noreply.github.com>
2021-08-25 01:11:44 +0200

    Pierwsza zmiana

--- JS-DOM/1/script.js
+++ JS-DOM/1/script.js
@@ -11,57 +11,6 @@ for (i = 0; i < cities.length; i++) {
 
     olElement.appendChild(liElement);
     console.log(olElement);
-    // newEl.innerText
 }
 
 document.body.appendChild(olElement);
-
-    // let olElement = document.createElement('ol');
-    // olElement.appendChild(liElement);
-
-//     console.log(count);
-
-
-
-//     let newEl = document.createElement('li'); 
-
-//     newEl.innerText = "Item " + count;
-//     console.log(newEl);
-
-//     list.appendChild(newEl);
-// }
-
-// wypisz();
-// olElement.setAttribute('class','city');
-// let olSelector = document.querySelector('ol');
-
-// olElement.innerHTML = cities;
-
-
-// for (let list of cities) {
-//     let liElement = document.createElement('li');
-//     let cityElement=liElement.cities
-//     console.log(cityElement);
-
-    //Tworzę elementy li dla kazdego z miast 
-    //Dodaje każdemu z tych elementów city 
-    //Ustawiam każdemu elementowi atrybut value i tekst w elemencie jako miasto
-    //Dopina element do listy ol 
-
-//     console.log(cities);
-// }
-
-// let pSelector = document.querySelector('p');
-// pSelector.innerHTML = cities;
-
-
-//Dopinam element listy do body
-
-
-
-
-/* ```
-kod na Discord
-``` */
-
-
